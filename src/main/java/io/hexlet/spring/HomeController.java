19f39c3c7648788b35be1055fdfdc53deb511225
d.orekh <d.orekh@tbank.ru>
2026-02-12 21:04:51 +0300

    Make project

---
+++ src/main/java/io/hexlet/spring/HomeController.java
@@ -0,0 +1,15 @@
+package io.hexlet.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Добро пожаловать в Hexlet Spring Blog!";
+    }
+}
